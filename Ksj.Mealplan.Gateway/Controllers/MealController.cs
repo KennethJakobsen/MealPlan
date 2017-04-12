4f4a83df9662034552a0ec494030e6b4b48419d1
Kenneth Jakobsen <kenneth.jakobsen@yahoo.com>
2017-04-12 11:11:02 +0200

    Added MealController

---
+++ Ksj.Mealplan.Gateway/Controllers/MealController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Ksj.Mealplan.Dtos;
+using Ksj.Mealplan.Messages;
+
+namespace Ksj.Mealplan.Gateway.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("meals")]
+    public class MealController : BaseApiController
+    {
+        public MealController() : base(new Uri("fabric:/Ksj.Mealplan/Service"), "groceries")
+        {
+        }
+        [Route("meal")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(CancellationToken cancelRequest)
+        {
+            return await SendAsync(this.Request, cancelRequest);
+        }
+
+        [Route("meal")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Create([FromBody]MealDto meal)
+        {
+            await Gateway.Bus.Publish(new AddMealMessage() { Meal = meal});
+            return Ok();
+        }
+
+        [Route("meal/search/{phrase}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Search(string phrase, CancellationToken cancelRequest)
+        {
+            return await SendAsync(this.Request, cancelRequest);
+        }
+    }
+}
