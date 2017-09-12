ebc9343e8f662522fc00b78161c5d23ba5d4faa7
Kenneth Jakobsen <kenneth.jakobsen@yahoo.com>
2017-09-12 12:42:12 +0200

    Added singleton time service to do some testing

--- Ksj.Mealplan.Notification/Hubs/TimeHub.cs
+++ Ksj.Mealplan.Notification/Hubs/TimeHub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Ksj.Mealplan.Notification.Services;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Ksj.Mealplan.Notification.Hubs
@@ -10,7 +11,6 @@ namespace Ksj.Mealplan.Notification.Hubs
     
     public class TimeHub : Hub
     {
-        private bool _publishTime = false;
         public TimeHub()
         {
 
@@ -19,13 +19,12 @@ namespace Ksj.Mealplan.Notification.Hubs
         public override Task OnConnectedAsync()
         {
             return base.OnConnectedAsync();
-            
         }
 
         public void StartTime()
         {
-            _publishTime = true;
-            while (_publishTime)
+            TimeService.Instance.PublishTime = true;
+            while (TimeService.Instance.PublishTime)
             {
                 base.Clients.All.InvokeAsync("Change", DateTime.UtcNow);
                 Thread.Sleep(100);
@@ -34,7 +33,7 @@ namespace Ksj.Mealplan.Notification.Hubs
         }
         public void StopTime()
         {
-            _publishTime = false;
+            TimeService.Instance.PublishTime = false;
 
         }
     }
---
+++ Ksj.Mealplan.Notification/Services/TimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksj.Mealplan.Notification.Services
+{
+    
+    public class TimeService
+    {
+        private static TimeService _instance;
+
+        private TimeService()
+        {
+        }
+
+        public static TimeService Instance => _instance ?? (_instance = new TimeService());
+        public bool PublishTime  { get; set; }  
+    }
+}
